71646a658de1c0b8d924e67aca554c827d7ae2dc
Pascal <pas111cal3@gmail.com>
2025-03-12 16:43:01 +0100

    aded techstack imgs

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
+let toggleBtnEl = document.getElementById('toggle-btn');
+let sideBarEl = document.getElementById('side-bar');
+
+function toggleSideBar(){
+  sideBarEl.classList.toggle('close')
+  toggleBtnEl.classList.toggle('rotate')
+
+  Array.from(sideBarEl.getElementsByClassName('show')).forEach(ul=>{
+    ul.classList.remove('show');
+     ul.previousElementSibling.classList.remove('rotate')
+   })
+}
+
 function toggleSubMenu(button){
    button.nextElementSibling.classList.toggle('show');
    button.classList.toggle('rotate');
+
+   if (sideBarEl.classList.toggle('close')){
+    sideBarEl.classList.toggle('close')
+  
+    
+   }
 }
 
