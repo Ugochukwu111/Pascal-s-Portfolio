cfb1b5a9a9cafada9b2e7baf89dd49d497b7d5c9
Pascal <pas111cal3@gmail.com>
2025-03-16 15:04:42 +0100

    dark mode feature done, site responsive

--- script.js
+++ script.js
@@ -17,10 +17,33 @@ function toggleSubMenu(button){
 
    if (sideBarEl.classList.toggle('close')){
     sideBarEl.classList.toggle('close')
-  
+    toggleBtnEl.classList.toggle('rotate')
     
    }
 }
+
+// theme js
+//geting current theme
+let darkmode = localStorage.getItem('darkmode');
+
+const themeBtn = document.querySelector('.theme-icons-cont')
+
+themeBtn.addEventListener('click', () =>{
+  darkmode = localStorage.getItem('darkmode')
+  darkmode !== 'active' ? enableDarkmode() : disableDarkmode()
+});
+
+function enableDarkmode(){
+  document.body.classList.add('darkmode')
+  localStorage.setItem('darkmode', 'active')
+}
+
+function disableDarkmode(){
+  document.body.classList.remove('darkmode')
+  localStorage.setItem('darkmode', null)
+}
+
+if (darkmode === 'active') enableDarkmode()
 
 
 
